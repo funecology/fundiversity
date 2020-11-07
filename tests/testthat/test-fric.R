7c7c812b30f8a4d4d1abe780470a172be3a676f1
Hugo Gruson <hugo.gruson@protonmail.com>
2020-11-07 10:31:16 +0100

    Merge FRic tests

--- tests/testthat/test-fric.R
+++ tests/testthat/test-fric.R
@@ -1,8 +1,25 @@
 # Preamble code
 data("traits_birds")
-traits_birds_sc = scale(traits_birds)
+traits_birds_sc <- scale(traits_birds)
 
 # Actual tests
+test_that("Functional Richness output format", {
+
+  fric <- expect_silent(fd_fric(traits_birds))
+
+  expect_type(fric, numeric)
+  expect_length(fric, 1)
+
+})
+
+test_that("Functional Richness works in 1D", {
+
+  fric <- expect_silent(fd_fric(traits_birds[, 1]))
+
+})
+
 test_that("Functional Richness computes correct value", {
+
   expect_equal(fd_fric(traits_birds_sc), 88.9286, tolerance = 1e-4)
+
 })
