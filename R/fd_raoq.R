6eecf7b4542dc4ed3b8b3ac3dcaf17529e9392c2
Matthias Grenié <matthias.grenie@ens-lyon.fr>
2020-11-11 14:47:22 +0100

    Make Rao QE work on multiple sites + tests

--- R/fd_raoq.R
+++ R/fd_raoq.R
@@ -1,7 +1,11 @@
 #' Compute Rao's entropy index (Rao's Q)
 #'
 #' @param traits The matrix dataset for which you want to compute the index
-#' @param sp_com Abundance matrix
+#' @param sp_com Site-species matrix with sites as rows and species as columns
+#'               if not provided, `fd_raoq()` considers all species with equal
+#'               abundance in a single site
+#' @param dist_matrix A dissimilarity matrix that can be provided instead of a
+#'                    trait data.frame
 #'
 #' @examples
 #' data(traits_birds)
@@ -16,7 +20,15 @@
 #' \doi{10.1007/s10651-005-1037-2}
 #'
 #' @export
-fd_raoq<- function(traits, sp_com) {
+fd_raoq <- function(traits, sp_com, dist_matrix = NULL) {
+
+  if ((!is.null(traits) & !is.null(dist_matrix)) |
+      (is.null(traits) & is.null(dist_matrix))) {
+    stop(
+      "Please provide either a trait dataset or a dissimilarity matrix",
+      call. = FALSE
+    )
+  }
 
   if (is.data.frame(traits)) {
     traits <- as.matrix(traits)
@@ -26,39 +38,43 @@ fd_raoq<- function(traits, sp_com) {
     traits <- as.matrix(traits)
   }
 
+  d <- dist_matrix
+
+  if (is.null(dist_matrix)) {
+    d <- as.matrix(dist(traits))
+  }
+
   if (!missing(sp_com)) {
 
-    abund <- setNames(
-      colSums(sp_com),
-      colnames(sp_com)
-    )
-
-    if (!all(names(traits) %in% names(abund))) {
+    if (!all(labels(d)[[1]] %in% colnames(sp_com))) {
       stop(
         "Please provide a sp_com matrix that contains all species ",
-        "from your traits dataset.", call. = FALSE
+        "from your traits dataset/dissimilarity matrix.", call. = FALSE
       )
     }
 
-    abund[match(abund, rownames(traits))] <- abund
+    sp_com <- sp_com[,labels(d)[[1]], drop = FALSE]
 
   } else {
 
-    abund <- setNames(
-      rep_len(1, nrow(traits)),
-      rownames(traits))
+    sp_com <- matrix(1, ncol = length(labels(d)[[1]]),
+                     dimnames = list("s1", labels(d)[[1]]))
 
   }
 
-  abund <- abund / sum(abund)
+  # Standardize abundance per site
+  sp_com <- sweep(sp_com, 1, rowSums(sp_com), "/")
 
-  d <- as.matrix(dist(traits))
+  # Compute Rao's Quadratic entropy over each row
+  q_site = apply(sp_com, 1, function(site_row) {
+    Q <- sum(combn(length(site_row), 2, function(ij) {
+      i <- ij[1]
+      j <- ij[2]
+      d[i,j] * site_row[i] * site_row[j]
+    }))
+  })
 
-  Q <- sum(combn(length(abund), 2, function(ij) {
-    i <- ij[1]
-    j <- ij[2]
-    d[i,j] * abund[i] * abund[j]
-  }))
-
-  return(Q)
+ data.frame(site = row.names(sp_com),
+            Q = q_site,
+            row.names = NULL)
 }
--- tests/testthat/test-rao.R
+++ tests/testthat/test-rao.R
@@ -1,22 +1,49 @@
 # Preamble code
 data("traits_birds")
-traits_birds_sc <- scale(traits_birds)
+simple_site_sp = matrix(1, nrow = 1, ncol = nrow(traits_birds),
+                        dimnames = list("s1", row.names(traits_birds)))
 
 # Actual tests
 test_that("Rao's entropy output format", {
 
-  rq <- expect_silent(fd_fdiv(traits_birds))
+  rq <- expect_silent(fd_raoq(traits_birds, sp_com = simple_site_sp))
 
-  expect_type(rq, "double")
-  expect_length(rq, 1)
+  expect_s3_class(rq, "data.frame")
+  expect_length(rq, 2)
+  expect_equal(nrow(rq), 1)
+  expect_equal(colnames(rq), c("site", "Q"))
+
+
+  rq <- expect_silent(fd_raoq(traits_birds))
+
+  expect_s3_class(rq, "data.frame")
+  expect_length(rq, 2)
+  expect_equal(nrow(rq), 1)
+  expect_equal(colnames(rq), c("site", "Q"))
 
 })
 
-test_that("Functional Divergence works in 1D", {
+test_that("Rao's entropy works in 1D", {
 
   expect_identical(
-    fd_raoq(traits_birds[, 1]),
-    fd_raoq(traits_birds[, 1, drop = FALSE])
+    fd_raoq(traits_birds[, 1], sp_com = simple_site_sp),
+    fd_raoq(traits_birds[, 1, drop = FALSE], sp_com = simple_site_sp)
   )
 
+})
+
+test_that("Rao's entropy fails gracefully", {
+  # No traits and no dissimilarity
+  expect_error(
+    fd_raoq(NULL, matrix(1), NULL),
+    "Please provide either a trait dataset or a dissimilarity matrix",
+    fixed = TRUE
+  )
+
+  # Both traits AND dissimilarity
+  expect_error(
+    fd_raoq(data.frame(a = 1), matrix(1), matrix(1)),
+    "Please provide either a trait dataset or a dissimilarity matrix",
+    fixed = TRUE
+  )
 })
