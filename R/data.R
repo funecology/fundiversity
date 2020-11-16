cd191990f23f8be94f499618085e4e4f0d48f21d
Matthias Grenié <matthias.grenie@ens-lyon.fr>
2020-11-16 13:33:38 +0100

    Document datasets #5

---
+++ R/data.R
@@ -0,0 +1,47 @@
+#' Functional Traits of Frugivorous Birds along a Tropical Gradient
+#'
+#' A dataset containing some functional traits of frugivorous birds in the Manú
+#' biosphere reserve, southeast Peru. Given are species mean trait values.
+#' The row names of the dataset give species names. Morphological
+#' traits have been measured on museum specimen following Eck et al.(2011).
+#' Traits have been measured only for adult and, if possible, for a minimum of
+#' two female and two male specimens. Body mass was taken
+#' from Dunning et al. (2007).
+#'
+#' @format A data frame with 217 rows and 4 variables:
+#' \describe{
+#'   \item{Bill.width..mm.}{bill width, in mm}
+#'   \item{Bill.length..mm.}{bill length, in mm}
+#'   \item{Kipp.s.index}{Kipp's index indicating wing Pointedness}
+#'   \item{Bodymass..g.}{adult's bodymass, in g}
+#' }
+#' @source Nowak, Larissa et al. (2019), Data from: Projecting consequences of
+#' global warming for the functional diversity of fleshy-fruited plants and
+#' frugivorous birds along a tropical elevational gradient, Dryad, Dataset,
+#' <doi:10.5061/dryad.c0n737b>
+"traits_birds"
+
+#' Functional Traits of Fleshy-fruit plants along a Tropical Gradient
+#'
+#' Taxonomy and functional traits of 392 fleshy-fruited plant species from
+#' the Manu National Park in south-east Peru. Given are fruit length and
+#' width (mm), plant height (m) and crop mass (g). Fruit traits have been
+#' measured on fresh fruit samples. Number of fruits per plant
+#' (used to determine the crop mass) and plant height have been estimated
+#' in the field. Given are species mean trait values, except for 23 species
+#' for which no species-level data was available and genus mean trait values
+#' are given (see column "Genus mean"). Species names are indicated as
+#' row names.  Plant.height..m. Crop.mass..g.
+#'
+#' @format A data frame with 392 rows and 4 variables:
+#' \describe{
+#'   \item{Fruit.length..mm.}{fruit length, in mm}
+#'   \item{Fruit.width..mm.}{fruit width, in mm}
+#'   \item{Plant.height..m.}{plant height, in m}
+#'   \item{Crop.mass..g.}{seed mass, in g}
+#' }
+#' @source Nowak, Larissa et al. (2019), Data from: Projecting consequences of
+#' global warming for the functional diversity of fleshy-fruited plants and
+#' frugivorous birds along a tropical elevational gradient, Dryad, Dataset,
+#' <doi:10.5061/dryad.c0n737b>
+"traits_plants"
