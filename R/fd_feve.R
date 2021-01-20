2868e3a784793d419b6279dc057840633f441a08
Rekyt <matthias.grenie@ens-lyon.fr>
2021-01-20 19:25:46 +0100

    First implementation of FEve fix #2

---
+++ R/fd_feve.R
@@ -0,0 +1,93 @@
+#' Compute Functional Evenness (FEve)
+#'
+#' @inheritParams fd_raoq
+#'
+#' @examples
+#' data(traits_birds)
+#' fd_feve(traits_birds)
+#'
+#' @return a data.frame with two columns:
+#' * `site` character column that contains site names based on input `sp_com`
+#' row names,
+#' * `FEve` numeric column that contains FEve values corresponding to each site.
+#'
+#' @references
+#' Villéger, S., Mason, N.W.H., Mouillot, D., 2008. New Multidimensional
+#' Functional Diversity Indices for a Multifaceted Framework in Functional
+#' Ecology. Ecology 89, 2290–2301. \doi{10.1890/07-1206.1}
+#'
+#' @importFrom stats dist
+#'
+#' @export
+fd_feve <- function(traits = NULL, sp_com, dist_matrix = NULL) {
+
+  if ((!is.null(traits) & !is.null(dist_matrix)) |
+      (is.null(traits) & is.null(dist_matrix))) {
+    stop(
+      "Please provide either a trait dataset or a dissimilarity matrix",
+      call. = FALSE
+    )
+  }
+
+  if (is.data.frame(traits) | is.vector(traits)) {
+    traits <- as.matrix(traits)
+  }
+
+  if (is.null(dist_matrix)) {
+    traits <- remove_species_without_trait(traits)
+
+    dist_matrix <- dist(traits)
+  }
+
+  dist_matrix <- as.matrix(dist_matrix)
+
+  if (!missing(sp_com)) {
+
+    common_species <- species_in_common(dist_matrix, sp_com)
+
+    dist_matrix <- dist_matrix[common_species, common_species, drop = FALSE]
+    sp_com      <- sp_com[, common_species, drop = FALSE]
+
+  } else {
+
+    sp_com <- matrix(1, ncol = nrow(dist_matrix),
+                     dimnames = list("s1", rownames(dist_matrix)))
+
+  }
+
+  # Standardize abundance per site
+  sp_com <- sp_com / rowSums(sp_com)
+
+  feve_site <- apply(sp_com, 1, function(site_row) {
+    fd_feve_single(site_row, dist_matrix)
+  })
+
+  data.frame(site = row.names(sp_com),
+             FEve = feve_site,
+             row.names = NULL)
+}
+
+# Hide gory details of computing single FEve values
+fd_feve_single = function(site_row, dist_matrix) {
+
+  if (sum(site_row > 0) < 3) {
+    FEve = NA_real_
+  } else {
+    present_species = names(site_row)[site_row > 0]
+
+    min_span_tree = vegan::spantree(dist_matrix[present_species, present_species])
+
+    one_over_s_minus_one = 1/(min_span_tree$n - 1)
+
+    ew = min_span_tree$dist /
+      (site_row[min_span_tree$labels[seq_along(min_span_tree$kid) + 1]] +
+         site_row[min_span_tree$labels[min_span_tree$kid]])
+
+    pew = ew/sum(ew)
+
+    FEve = (sum(pmin(pew, one_over_s_minus_one)) -
+              one_over_s_minus_one)/(1 - one_over_s_minus_one)
+  }
+
+  return(FEve)
+}
---
+++ tests/testthat/test-feve.R
@@ -0,0 +1,91 @@
+# Preamble code
+data("traits_birds")
+simple_site_sp = matrix(1, nrow = 1, ncol = nrow(traits_birds),
+                        dimnames = list("s1", row.names(traits_birds)))
+
+# Actual tests
+test_that("Functional Evenness output format", {
+
+  feve <- expect_silent(fd_feve(traits_birds, sp_com = simple_site_sp))
+
+  expect_s3_class(feve, "data.frame")
+  expect_length(feve, 2)
+  expect_equal(nrow(feve), 1)
+  expect_equal(colnames(feve), c("site", "FEve"))
+
+
+  feve <- expect_silent(fd_feve(traits_birds))
+
+  expect_s3_class(feve, "data.frame")
+  expect_length(feve, 2)
+  expect_equal(nrow(feve), 1)
+  expect_equal(colnames(feve), c("site", "FEve"))
+
+})
+
+test_that("Functional Evenness computation are in line with other packages", {
+
+  # With internal dataset
+  expect_equal(fd_feve(traits_birds, simple_site_sp)$FEve, 0.3743341,
+               tolerance = 1e-6)
+
+  # With perfectly spread dataset
+  test_dissim  = matrix(c(
+    0, 1, 2,
+    1, 0, 1,
+    2, 1, 0
+  ),
+  byrow = TRUE, ncol = 3, dimnames = list(letters[1:3], letters[1:3]))
+
+  abund_mat = matrix(1, ncol = 3, dimnames = list("site1", letters[1:3]))
+
+  expect_equal(fd_feve(sp_com = abund_mat, dist_matrix = test_dissim)$FEve, 1)
+})
+
+test_that("Functional Evenness works in 1D", {
+
+  expect_identical(
+    fd_feve(traits_birds[, 1], sp_com = simple_site_sp),
+    fd_feve(traits_birds[, 1, drop = FALSE], sp_com = simple_site_sp)
+  )
+
+})
+
+test_that("Functional Evenness works on subset of site/species", {
+  site_sp <- matrix(1, ncol = nrow(traits_birds))
+  colnames(site_sp) <-  rownames(traits_birds)
+  rownames(site_sp) <- "s1"
+
+  expect_message(fd_feve(traits_birds, site_sp[, 2:ncol(site_sp),
+                                               drop = FALSE]),
+                 paste0("Differing number of species between trait dataset ",
+                        "and site-species matrix\nTaking subset of species"))
+
+  expect_message(fd_feve(traits_birds[2:nrow(traits_birds),], site_sp),
+                 paste0("Differing number of species between trait dataset ",
+                        "and site-species matrix\nTaking subset of species"))
+})
+
+test_that("Functional Evenness fails gracefully", {
+  # No traits and no dissimilarity
+  expect_error(
+    fd_feve(NULL, matrix(1), NULL),
+    "Please provide either a trait dataset or a dissimilarity matrix",
+    fixed = TRUE
+  )
+
+  # Both traits AND dissimilarity
+  expect_error(
+    fd_feve(data.frame(a = 1), matrix(1), matrix(1)),
+    "Please provide either a trait dataset or a dissimilarity matrix",
+    fixed = TRUE
+  )
+
+  # Species matrix doesn't contain species from trait data
+  expect_error(
+    fd_feve(data.frame(a = 1, row.names = "sp1"), matrix(1)),
+    paste0("No species in common found between trait dataset ",
+           "and site-species matrix"),
+    fixed = TRUE
+  )
+})
