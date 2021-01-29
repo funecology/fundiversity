76b24a80b1ff09e268925d22ae5dcf2dec926dde
Matthias Grenié <matthias.grenie@ens-lyon.fr>
2021-01-29 15:19:08 +0100

    Implement sparse matrices for Rao's QE

--- tests/testthat/test-raoq.R
+++ tests/testthat/test-raoq.R
@@ -52,6 +52,20 @@ test_that("Rao's Quadratic Entropy works on subset of site/species", {
                         "and site-species matrix\nTaking subset of species"))
 })
 
+test_that("Rao's Quadratic Entropy works on sparse matrices", {
+  site_sp <- matrix(1, ncol = nrow(traits_birds))
+  colnames(site_sp) <-  rownames(traits_birds)
+  rownames(site_sp) <- "s1"
+
+  sparse_site_sp <- as(site_sp, "sparseMatrix")
+
+  expect_silent(fd_raoq(traits_birds, sparse_site_sp))
+
+  expect_equal(fd_raoq(traits_birds, sparse_site_sp)$Q, 170.0519,
+               tolerance = 1e-6)
+
+})
+
 test_that("Rao's entropy fails gracefully", {
   # No traits and no dissimilarity
   expect_error(
