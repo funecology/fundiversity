982078765d1f7d7df1a5bdf1486169f4aded32cd
Matthias Grenié <matthias.grenie@ens-lyon.fr>
2021-02-15 16:00:27 +0100

    Conditional memoise test

--- tests/testthat/test-chull.R
+++ tests/testthat/test-chull.R
@@ -1,6 +1,8 @@
 test_that("memoised fd_chull works", {
   data("traits_birds")
 
-  ch <- fd_chull_memoised(traits_birds)
-  expect_true(memoise::has_cache(fd_chull_memoised)(traits_birds))
+  ch <- fd_chull(traits_birds)
+  if (requireNamespace("memoise", quietly = TRUE)) {
+    expect_true(memoise::has_cache(fd_chull)(traits_birds))
+  }
 })
