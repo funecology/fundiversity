0e3ea0014cbf7fb0c426194770b94feba85198f2
Hugo Gruson <hugo.gruson@protonmail.com>
2021-02-16 19:14:28 +0100

    Turn roxygen comment into normal comment to avoid creation of a Rd file

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,6 @@
 # Diverse utility functions
 
-#' Memoized version of fd_chull loaded if package is installed
+# Memoized version of fd_chull loaded if package is installed
 .onLoad <- function(pkgname, libname) {
   if (requireNamespace("memoise", quietly = TRUE)) {
     fd_chull <<- memoise::memoise(fd_chull)
