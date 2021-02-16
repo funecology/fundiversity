d85722e57242f1ec2ba9377f5999d14dcad1e71a
Rekyt <matthias.grenie@ens-lyon.fr>
2021-02-16 11:55:20 +0100

    first test if memoised then test cache

--- tests/testthat/test-chull.R
+++ tests/testthat/test-chull.R
@@ -1,8 +1,9 @@
 test_that("memoised fd_chull works", {
   skip_if_not_installed("memoise")
 
-  data("traits_birds")
+  expect_true(memoise::is.memoised(fd_chull))
 
+  data("traits_birds")
   ch <- fd_chull(traits_birds)
 
   expect_true(memoise::has_cache(fd_chull)(traits_birds))
