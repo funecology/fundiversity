8f63ca7d35d16e129dc5d483a9565d5da8141ead
Rekyt <matthias.grenie@ens-lyon.fr>
2021-03-02 16:19:48 +0100

    Try testing optional memoization with option

--- tests/testthat/test-chull.R
+++ tests/testthat/test-chull.R
@@ -7,4 +7,26 @@ test_that("memoised fd_chull works", {
   ch <- fd_chull(traits_birds)
 
   expect_true(memoise::has_cache(fd_chull)(traits_birds))
+
+  # Explicitly set up the memoise option to FALSE
+  options(fundiversity.memoise = FALSE)
+  expect_false(getOption("fundiversity.memoise", TRUE))
+
+  fundiversity:::.onLoad(system.file(package = "fundiversity"), "fundiversity")
+  expect_false(memoise::is.memoised(fd_chull))
+
+  ch2 <- fd_chull(traits_birds[2:5,])
+
+  expect_false(memoise::has_cache(fd_chull)(traits_birds[2:5,]))
+
+  # Reset the option to TRUE
+  options(fundiversity.memoise = TRUE)
+  expect_true(getOption("fundiversity.memoise", TRUE))
+
+  fundiversity:::.onLoad(system.file(package = "fundiversity"), "fundiversity")
+  expect_true(memoise::is.memoised(fd_chull))
+
+  ch2 <- fd_chull(traits_birds[2:5,])
+
+  expect_true(memoise::has_cache(fd_chull)(traits_birds[2:5,]))
 })
