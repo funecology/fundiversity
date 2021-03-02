5a461653dcc618814a6bad6148d0094d678fa0ef
Rekyt <matthias.grenie@ens-lyon.fr>
2021-03-02 15:40:26 +0100

    Add missing trait value

--- tests/testthat/test-chull_intersect.R
+++ tests/testthat/test-chull_intersect.R
@@ -4,7 +4,7 @@ test_that("memoised fd_chull_intersect works", {
   expect_true(memoise::is.memoised(fd_chull_intersect))
 
   data("traits_birds")
-  ch <- fd_chull_intersect(traits_birds)
+  ch <- fd_chull_intersect(traits_birds, traits_birds)
 
-  expect_true(memoise::has_cache(fd_chull_intersect)(traits_birds))
+  expect_true(memoise::has_cache(fd_chull_intersect)(traits_birds, traits_birds))
 })
