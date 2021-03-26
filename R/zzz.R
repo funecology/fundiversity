a4d89967b3806cc2c70e3d926691ef8ea445968d
Hugo Gruson <hugo.gruson@protonmail.com>
2021-03-26 17:55:08 +0100

    Wrap getOption() with isTRUE() to deal with the case when users enter fundiversity.memoise = foo

--- R/zzz.R
+++ R/zzz.R
@@ -3,7 +3,7 @@
 # Memoized version of fd_chull loaded if package is installed
 .onLoad <- function(libname, pkgname) {
   if (requireNamespace("memoise", quietly = TRUE) &
-      getOption("fundiversity.memoise", TRUE)) {
+      isTRUE(getOption("fundiversity.memoise", TRUE))) {
     fd_chull <<- memoise::memoise(fd_chull)
     fd_chull_intersect <<- memoise::memoise(fd_chull_intersect)
   }
