b6fb7c15ca21810e5810e070f50b20ccfc05e77f
Hugo Gruson <hugo.gruson@protonmail.com>
2021-03-26 17:09:22 +0100

    Test memoise option by adding another action worker

--- tests/testthat/test-chull.R
+++ tests/testthat/test-chull.R
@@ -1,27 +1,20 @@
+data("traits_birds")
+
 test_that("memoise options works on fd_chull()", {
   skip_if_not_installed("memoise")
 
+  if (getOption("fundiversity.memoise", TRUE)) {
     expect_true(memoise::is.memoised(fd_chull))
 
-  data("traits_birds")
-  ch <- fd_chull(traits_birds)
+    ch <- fd_chull(traits_birds)
 
-  expect_true(memoise::has_cache(fd_chull)(traits_birds))
+    expect_true(memoise::has_cache(fd_chull)(traits_birds))
+  } else {
+    expect_false(memoise::is.memoised(fd_chull))
 
-  # Explicitly set up the memoise option to FALSE
-  options(fundiversity.memoise = FALSE)
-  expect_false(getOption("fundiversity.memoise", TRUE))
+    ch <- fd_chull(traits_birds)
 
-  fundiversity:::.onLoad(system.file(package = "fundiversity"), "fundiversity")
-  expect_false(memoise::is.memoised(fd_chull))
+    expect_false(memoise::has_cache(fd_chull)(traits_birds))
+  }
 
-  # Reset the option to TRUE
-  options(fundiversity.memoise = TRUE)
-  expect_true(getOption("fundiversity.memoise", TRUE))
-
-  fundiversity:::.onLoad(system.file(package = "fundiversity"), "fundiversity")
-  expect_true(memoise::is.memoised(fd_chull))
-  ch2   <- fd_chull(traits_birds[2:5,])
-
-  expect_true(memoise::has_cache(fd_chull)(traits_birds[2:5,]))
 })
--- tests/testthat/test-chull_intersect.R
+++ tests/testthat/test-chull_intersect.R
@@ -1,32 +1,24 @@
+data("traits_birds")
+
 test_that("memoise options work on fd_chull_intersect()", {
   skip_if_not_installed("memoise")
 
-  expect_true(memoise::is.memoised(fd_chull_intersect))
+  if (getOption("fundiversity.memoise", TRUE)) {
+    expect_true(memoise::is.memoised(fd_chull_intersect))
 
-  data("traits_birds")
-  ch_i <- fd_chull_intersect(traits_birds, traits_birds)
+    ch_i <- fd_chull_intersect(traits_birds, traits_birds)
 
-  expect_true(
-    memoise::has_cache(fd_chull_intersect)(traits_birds, traits_birds)
-  )
+    expect_true(
+      memoise::has_cache(fd_chull_intersect)(traits_birds, traits_birds)
+    )
+  } else {
+    expect_false(memoise::is.memoised(fd_chull_intersect))
 
-  options(fundiversity.memoise = FALSE)
-  expect_false(getOption("fundiversity.memoise", TRUE))
+    ch_i <- fd_chull_intersect(traits_birds, traits_birds)
 
-  fundiversity:::.onLoad(system.file(package = "fundiversity"), "fundiversity")
-  expect_false(memoise::is.memoised(fd_chull_intersect))
+    expect_false(
+      memoise::has_cache(fd_chull_intersect)(traits_birds, traits_birds)
+    )
+  }
 
-  # Reset the option to TRUE
-  options(fundiversity.memoise = TRUE)
-  expect_true(getOption("fundiversity.memoise", TRUE))
-
-  fundiversity:::.onLoad(system.file(package = "fundiversity"), "fundiversity")
-  expect_true(memoise::is.memoised(fd_chull_intersect))
-
-  ch_i2 <- fd_chull_intersect_memoised(traits_birds[2:5,], traits_birds[2:5,])
-
-  expect_true(
-    memoise::has_cache(fd_chull_intersect)(traits_birds[2:5,],
-                                                    traits_birds[2:5,])
-  )
 })
