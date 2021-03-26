de7c8852ff0486e442209f95cc3c13bfb012c050
Matthias Grenié <matthias.grenie@ens-lyon.fr>
2021-03-26 23:41:45 +0100

    Option to toggle off memoization - fix #31 (#33)
    Co-authored-by: Hugo Gruson <hugo.gruson@protonmail.com>

--- tests/testthat/test-chull.R
+++ tests/testthat/test-chull.R
@@ -1,10 +1,20 @@
-test_that("memoised fd_chull works", {
+data("traits_birds")
+
+test_that("memoise options works on fd_chull()", {
   skip_if_not_installed("memoise")
 
-  expect_true(memoise::is.memoised(fd_chull))
+  if (getOption("fundiversity.memoise", TRUE)) {
+    expect_true(memoise::is.memoised(fd_chull))
 
-  data("traits_birds")
-  ch <- fd_chull(traits_birds)
+    ch <- fd_chull(traits_birds)
 
-  expect_true(memoise::has_cache(fd_chull)(traits_birds))
+    expect_true(memoise::has_cache(fd_chull)(traits_birds))
+  } else {
+    expect_false(memoise::is.memoised(fd_chull))
+
+    ch <- fd_chull(traits_birds)
+
+    expect_false(memoise::has_cache(fd_chull)(traits_birds))
+  }
+
 })
